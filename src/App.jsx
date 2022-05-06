57e1dfedf6cb4640f7100fdb513190e5dfee6a5b
Ivan Otta <Ivanotta18@gmail.com>
2022-05-06 14:38:42 -0300

    ItenCount agregado

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,32 @@
 import "./css/Styles.css"
 import NavBar from './componentes/NavBar';
 import ItemListContainer from "./componentes/ItemListContainer";
+import ItemCount from "./componentes/ItemCount";
+
+
+
 
 
 function App() {
+
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={'Bienvenido!'} />
+      <ItemCount stock={5} initial={1} onAdd={(cantidad) => console.log(`${cantidad} Productos Agregados`)} /> 
+
+
+
+
+
+
+
+
     </div>
+
+
+
+
   );
 }
 
---
+++ src/componentes/ItemCount.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function ItemCount({ stock, initial, onAdd }) {
+    const [cantidad, setCantidad] = useState(initial)
+
+    function contadorS() {
+        if (cantidad < stock) {
+            setCantidad(cantidad + 1)
+        }
+    }
+    function contadorR() {
+        if (cantidad > 1) {
+            setCantidad(cantidad - 1)
+        }
+    }
+
+    function agregadoAlCarrito() {
+        onAdd(cantidad)
+    }
+
+
+
+    return (
+        <div>
+            <div className="boton" > 
+                <button className="botonS" onClick={contadorS} > + </button>
+                <h3> {cantidad} </h3>
+                <button className="botonR" onClick={contadorR} > - </button>
+                <button className="btnAddProductos" onClick={agregadoAlCarrito} >Agregar productos</button>
+            </div>
+        </div>
+
+    )
+}
