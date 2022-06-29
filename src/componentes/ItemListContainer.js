027d6eb1da24d2e615f910f483d988eb88441684
Ivan Otta <Ivanotta18@gmail.com>
2022-06-29 15:15:58 -0300

    actualizados

---
+++ src/componentes/ItemListContainer.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Ring } from "@uiball/loaders";
+import { useParams } from "react-router-dom";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import ItemList from "./ItemList";
+
+<Ring size={40} lineWeight={5} />;
+
+function ItemListContainer() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const db = getFirestore();
+    const QueryCollection = collection(db, "items");
+
+    if (id) {
+      getDocs(QueryCollection)
+        .then((resp) =>
+          setProducts(
+            resp.docs
+              .map((item) => ({ id: item.id, ...item.data() }))
+              .filter((prods) => prods.categoria === id)
+          )
+        
+        )
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    } else {
+      getDocs(QueryCollection)
+        .then((resp) =>
+          setProducts(
+            resp.docs.map((item) => ({ id: item.id, ...item.data() })))   
+        )
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    }
+  }, [id]);
+  
+  return (
+    <div>
+      <div className="bienvenida">
+        {loading ? <Ring color={"red"} /> : <ItemList products={products} />}
+      </div>
+    </div>
+  );
+}
+
+export default ItemListContainer;
